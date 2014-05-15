9b87085946a99d9c1f448e814602c45c89378264
qinfanpeng <qinfanpeng@gmail.com>
2014-05-15 20:41:23 +0800

    change the rspec utilts.rb

--- spec/support/utilies.rb
+++ spec/support/utilies.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 def sign_in_with(user)
   visit new_user_session_path
-  fill_in 'Email',  with: user.email
-  fill_in 'Password',   with: user.password
-  click_button 'Sign in'
+  fill_in '邮箱地址',  with: user.email
+  fill_in '密码',   with: user.password
+  click_button '登录'
 end
